7c2990be3ef482dd505a7157cf549632ab88ffb6
Jia-Baos <18383827268@163.com>
2024-11-02 21:13:59 +0800

    2024-11-02 21:13

--- src/MiniCircle/MiniCircle.cpp
+++ src/MiniCircle/MiniCircle.cpp
@@ -2,7 +2,7 @@
 
 // 判断半径大小
 int Judge(const double a, const double b) {
-  if (std::abs(a - b) < eps) {
+  if (std::abs(a - b) < kEps) {
     return 0;
   }
   if (a < b) {
@@ -12,22 +12,22 @@ int Judge(const double a, const double b) {
 }
 
 // 向量旋转，a表示顺时针旋转的弧度值
-MyVector Rotate(const MyVector &p1, const double a) {
-  return {p1.my_x * cos(a) + p1.my_y * sin(a), -p1.my_x * sin(a) + p1.my_y * cos(a)};
+Point Rotate(const Point &p1, const double a) {
+  return {p1.x * std::cos(a) + p1.y * std::sin(a), -p1.x * std::sin(a) + p1.y * std::cos(a)};
 }
 
 // 求两点之间距离
-double Distance(const MyVector &p1, const MyVector &p2) {
-  const double dx = p1.my_x - p2.my_x;
-  const double dy = p1.my_y - p2.my_y;
+double Distance(const Point &p1, const Point &p2) {
+  const double dx = p1.x - p2.x;
+  const double dy = p1.y - p2.y;
   return std::sqrt(dx * dx + dy * dy);
 }
 
 // 求两直线的交点
-MyVector Intersection(const MyVector &p1, const MyVector &v1, const MyVector &p2, const MyVector &v2) {
-  MyVector result;
+Point Intersection(const Point &p1, const Point &v1, const Point &p2, const Point &v2) {
+  Point result;
   if (v1 * v2 != 0) {
-    const MyVector temp = p1 - p2;
+    const Point temp = p1 - p2;
     const double ratio = (v2 * temp) / (v1 * v2);
     result = p1 + v1 * ratio;
   }
@@ -35,24 +35,24 @@ MyVector Intersection(const MyVector &p1, const MyVector &v1, const MyVector &p2
 }
 
 // 求两点之间连线的中垂线
-std::pair<MyVector, MyVector> MidPerpendicular(const MyVector &p1, const MyVector &p2) {
-  const MyVector mid = (p1 + p2) / 2;
+std::pair<Point, Point> MidPerpendicular(const Point &p1, const Point &p2) {
+  const Point mid = (p1 + p2) / 2;
   // 垂直时再利用旋转求解会降低精度
-  // const MyVector rotate = Rotate(p1 - p2, PI / 2);
-  const MyVector rotate = {-p1.my_y + p2.my_y, p1.my_x - p2.my_x};
+  // const Point rotate = Rotate(p1 - p2, PI / 2);
+  const Point rotate = {-p1.y + p2.y, p1.x - p2.x};
   return {mid, rotate};
 }
 
 // 已知三点求外接圆
-Circle MiniCircle(MyVector p1, MyVector p2, MyVector p3) {
+Circle MiniCircle(Point p1, Point p2, Point p3) {
   auto m1 = MidPerpendicular(p1, p2);
   auto m2 = MidPerpendicular(p1, p3);
-  MyVector center_point = Intersection(m1.my_x, m1.my_y, m2.my_x, m2.my_y);
+  Point center_point = Intersection(m1.first, m1.second, m2.first, m2.second);
   double radius = Distance(center_point, p1);
   return {center_point, radius};
 }
 
-Circle MinimumCoveringCircle(const std::vector<MyVector> &points, const int npoints) {
+Circle MinimumCoveringCircle(const std::vector<Point> &points, const int npoints) {
   // 初始化圆，圆心为points[0]，半径为0
   Circle circle(points[0], 0.0);
   for (int i = 1; i < npoints; i++) {
--- src/mini_circle_demo/main.cpp
+++ src/mini_circle_demo/main.cpp
@@ -17,24 +17,24 @@
 int main(int argc, char *argv[]) {
   std::cout << "hello world" << std::endl;
 
-  MyVector p1(2.0, 2.0);
-  MyVector v1(1.0, 1.0);
-  MyVector p2(2.0, -2.0);
-  MyVector v2(1.0, -1.0);
-  MyVector p3(0.0, 0.0);
+  Point p1(2.0, 2.0);
+  Point v1(1.0, 1.0);
+  Point p2(2.0, -2.0);
+  Point v2(1.0, -1.0);
+  Point p3(0.0, 0.0);
 
   double value;
   Circle circle;
-  std::pair<double, double> result;
-  std::pair<MyVector, MyVector> combination;
+  Point result;
+  std::pair<Point, Point> combination;
 
   // 加法测试
   result = p1 + p2;
-  std::cout << "(2.0,2.0) + (2.0,-2.0): " << result.my_x << " " << result.my_y << std::endl;
+  std::cout << "(2.0,2.0) + (2.0,-2.0): " << result.x << " " << result.y << std::endl;
 
   // 减法测试
   result = p1 - p2;
-  std::cout << "(2.0,2.0) - (2.0,-2.0): " << result.my_x << " " << result.my_y << std::endl;
+  std::cout << "(2.0,2.0) - (2.0,-2.0): " << result.x << " " << result.y << std::endl;
 
   // 叉乘测试
   value = p1 * p2;
@@ -42,19 +42,19 @@ int main(int argc, char *argv[]) {
 
   // 数乘测试
   result = p1 * 1.0;
-  std::cout << "(2.0,2.0) * 1.0: " << result.my_x << " " << result.my_y << std::endl;
+  std::cout << "(2.0,2.0) * 1.0: " << result.x << " " << result.y << std::endl;
 
   // 除法测试
   result = p1 / 1.0;
-  std::cout << "(2.0,2.0) / 1.0: " << result.my_x << " " << result.my_y << std::endl;
+  std::cout << "(2.0,2.0) / 1.0: " << result.x << " " << result.y << std::endl;
 
   // Judge函数测试
   value = Judge(2.0, 3.0);
   std::cout << "2.0, 3.0 (-1 means not int circle): " << value << std::endl;
 
   // 向量旋转测试
-  result = Rotate(v1, PI / 2);
-  std::cout << "(1.0,1.0) rotate PI/2: " << result.my_x << " " << result.my_y << std::endl;
+  result = Rotate(v1, kPi / 2);
+  std::cout << "(1.0,1.0) rotate PI/2: " << result.x << " " << result.y << std::endl;
 
   // 两点之间距离测试
   value = Distance(p1, p2);
@@ -62,24 +62,24 @@ int main(int argc, char *argv[]) {
 
   // 两直线之间交点测试
   result = Intersection(p1, v1, p2, v2);
-  std::cout << "p1(2.0,2.0), v1(1.0,1.0), p2(2.0,-2.0), v2(1.0,-1.0) Cross Point: " << result.my_x << " " << result.my_y
+  std::cout << "p1(2.0,2.0), v1(1.0,1.0), p2(2.0,-2.0), v2(1.0,-1.0) Cross Point: " << result.x << " " << result.y
             << std::endl;
 
   // 两点之间中垂线测试
   combination = MidPerpendicular(p1, p2);
-  std::cout << "p1(2.0,2.0), p2(2.0,-2.0) Mid Perpendicular: " << "Point: " << combination.my_x.my_x << " "
-            << combination.my_x.my_y << " Vec: " << combination.my_y.my_x << " " << combination.my_y.my_y << std::endl;
+  std::cout << "p1(2.0,2.0), p2(2.0,-2.0) Mid Perpendicular: " << "Point: " << combination.first.x << " "
+            << combination.first.y << " Vec: " << combination.second.x << " " << combination.second.y << std::endl;
 
   // 三点外接圆测试
   circle = MiniCircle(p1, p2, p3);
-  std::cout << "p1(2.0,2.0), p2(2.0,-2.0), p3(0.0,0.0) Mini Circle: " << "Point: " << circle.center.my_x << " "
-            << circle.center.my_y << " Radius: " << circle.radius << std::endl;
+  std::cout << "p1(2.0,2.0), p2(2.0,-2.0), p3(0.0,0.0) Mini Circle: " << "Point: " << circle.center.x << " "
+            << circle.center.y << " Radius: " << circle.radius << std::endl;
 
   OS_SLEEP(1);
 
   for (size_t i = 0; i < 10; ++i) {
     std::cout << "hello world" << std::endl;
   }
-  
+
   return 0;
 }
